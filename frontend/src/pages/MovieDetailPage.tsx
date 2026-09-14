ddb341aeea3417e56c647aa2b99edba433442fb7
RodrigoGR10 <114446626+RodrigoGR10@users.noreply.github.com>
2026-09-14 14:59:53 -0300

    Crea vista de detalle de pelicula

---
+++ frontend/src/pages/MovieDetailPage.tsx
@@ -0,0 +1,22 @@
+function MovieDetailPage() {
+    return (
+        <div>
+            <h1>Superman</h1>
+
+            <p>Duración: 130 minutos</p>
+            <p>Género: Acción</p>
+            <p>Clasificación: TE+7</p>
+
+            <h2>Funciones</h2>
+
+            <h3>Cinemark - Maipú</h3>
+            <p>18:30 - 2D - Subtitulada - $4500</p>
+            <p>21:00 - 3D - Doblada - $6000</p>
+
+            <h3>Cinépolis - La Reina</h3>
+            <p>19:15 - 2D - Subtitulada - $4800</p>
+        </div>
+    );
+}
+
+export default MovieDetailPage;
