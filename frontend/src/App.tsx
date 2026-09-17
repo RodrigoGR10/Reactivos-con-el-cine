4778c98595a11ebca6575cb0df8d4b624cc75057
RodrigoGR10 <114446626+RodrigoGR10@users.noreply.github.com>
2026-09-17 20:14:14 -0300

    feat: configura react-router y lee el id de la pelicula desde la URL

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,14 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MovieDetailPage from "./pages/MovieDetailPage";
 
 function App() {
-  return <MovieDetailPage />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/peliculas/:id" element={<MovieDetailPage />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
