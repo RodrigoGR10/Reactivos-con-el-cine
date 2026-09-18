f842ede7240351558670446f53badaf3cbfc3c52
RodrigoGR10 <114446626+RodrigoGR10@users.noreply.github.com>
2026-09-17 23:35:40 -0300

    fix: corrige tipos de pelicula

--- frontend/src/types/types.ts
+++ frontend/src/types/types.ts
@@ -1,24 +1,26 @@
 export interface Pelicula {
-    id: number
-    titulo: string
-    duracion?: number
-    genero?: string
-    clasificacion?: string
-    sinopsis?: string
-    poster?: string
+    id: number;
+    titulo: string;
+    duracion: number;
+    genero: string;
+    clasificacion: string;
+    sinopsis: string;
+    poster: string;
 }
+
 export interface Cine {
-    id: number
-    nombre: string
-    comuna: string
-    logo?: string
+    id: number;
+    nombre: string;
+    comuna: string;
+    logo?: string;
 }
+
 export interface Funcion {
-    id: number
-    peliculaId: number
-    cineId: number
-    horario: string
-    formato: string
-    idioma: string
-    precio: number
+    id: number;
+    peliculaId: number;
+    cineId: number;
+    horario: string;
+    formato: string;
+    idioma: string;
+    precio: number;
 }
