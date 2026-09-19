23f14bfe15b2c9ac5c4d783fdac2cad7bb5059b5
cris <cristobalaravena751@gmail.com>
2026-09-19 18:12:24 -0300

    feat: implementado boton de peli favorita para vista principal

--- frontend/src/components/TarjetaPelicula.tsx
+++ frontend/src/components/TarjetaPelicula.tsx
@@ -1,13 +1,26 @@
 import {Link} from 'react-router-dom'
 import type { Pelicula } from '../types/types.ts'
-
+import { useFavorites } from '../context/FavoritesContext.tsx'
+import { useAuth } from '../context/AuthContext.tsx'
 interface TarjetaPeliculaProps {
   pelicula: Pelicula
 }
 
 function TarjetaPelicula({ pelicula }: TarjetaPeliculaProps) {
+
+  const { user, openAuthModal } = useAuth();
+  const { isFavoriteMovie, toggleFavoriteMovie } = useFavorites();
+
+  const handleToggleFavorito = () => {
+    if (!user) {
+      openAuthModal();
+      return;
+    }
+    toggleFavoriteMovie(Number(pelicula.id));
+  };
   return (
     <article className="tarjeta-pelicula">
+      <div className="poster-pelicula">
       {pelicula.poster ? (
         <img src={pelicula.poster} alt={`Póster de ${pelicula.titulo}`} />
       ) : (
@@ -16,7 +29,17 @@ function TarjetaPelicula({ pelicula }: TarjetaPeliculaProps) {
           <small>{pelicula.titulo}</small>
         </div>
       )}
-
+      <div className="overlay-fav">
+                <button
+                  type="button"
+                  className="boton-fav"
+                  onClick={handleToggleFavorito}
+                  aria-label={isFavoriteMovie(Number(pelicula.id)) ? 'Quitar de favoritos' : 'Añadir a favoritos'}
+                >
+                  {isFavoriteMovie(Number(pelicula.id)) ? '♥' : '♡'}
+                </button>
+              </div>
+          </div>
       <div className="contenido-tarjeta">
         <h3>{pelicula.titulo}</h3>
         <p className="datos-pelicula">
